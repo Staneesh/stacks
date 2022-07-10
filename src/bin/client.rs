89601d74de7092b220f89a81271c104f85d957b1
Staneesh <blkreasons@outlook.com>
2022-07-10 21:28:39 +0200

    :sparkles: REST

--- src/bin/client.rs
+++
@@ -1,8 +0,0 @@
-use std::net::UdpSocket;
-
-fn main() {
-    let socket = UdpSocket::bind("0.0.0.0:8080").expect("Couldn't bind a socket!");
-    socket
-        .send_to(&[0; 10], "70.34.254.33:8080")
-        .expect("Didn't send!");
-}
--- src/bin/server.rs
+++
@@ -1,9 +0,0 @@
-use std::net::UdpSocket;
-
-fn main() {
-    let socket = UdpSocket::bind("0.0.0.0:8080").expect("Couldn't bind a socket!");
-    let mut buf = [0; 100];
-    let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data!");
-    let filled_buf = &mut buf[..number_of_bytes];
-    println!("BUF:{:?}\n", filled_buf);
-}
